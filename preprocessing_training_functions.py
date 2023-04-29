c0376bf2d66a2bcee154cd76820632216be78233
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2023-04-29 17:46:42 -0400

    Update preprocessing_training_functions.py

--- preprocessing_training_functions.py
+++ preprocessing_training_functions.py
@@ -130,70 +130,12 @@ def scrape_statistics(stat_df, situation='ev', stat_type='std', download_file=Fa
 
     return stat_df
 
-def create_instance_df(stat_df, download_file=False):
-    start_year = 2007
-    end_year = 2023
-
-    # Age calculated as of the October 1st of the season
-    instance_df = pd.DataFrame(index=['Instance ID'], columns=['Player', 'Year', 'Position', 'Age', 'Height', 'Weight', 'Draft Position', 'Y1 GP', 'Y2 GP', 'Y3 GP', 'Y4 GP', 'Y5 GP', 'Y1 EV ATOI', 'Y2 EV ATOI', 'Y3 EV ATOI', 'Y4 EV ATOI', 'Y5 EV ATOI'])
-    for index, row in stat_df.iterrows():
-        for year in range(start_year, end_year):
-            if pd.isnull(error_catch_input_data(row, year, 5, None, 'GP')):
-                pass
-            else:
-
-                print(f'{len(instance_df)} / 14335')
-
-                # Age calculation
-                dob = date(int(row['Date of Birth'].split('-')[0]), int(row['Date of Birth'].split('-')[1]), int(row['Date of Birth'].split('-')[2])) #year, month, date
-                target_date = date(year, 10, 1)
-                delta_days = target_date - dob
-                age = round(delta_days.days/365.24,3)
-
-                instance_df.loc[f"{row['Player']} {year+1}"] = [row['Player'], 
-                                                                year+1, row['Position'], 
-                                                                age, 
-                                                                row['Height (in)'], 
-                                                                row['Weight (lbs)'], 
-                                                                row['Overall Draft Position'], 
-                                                                error_catch_input_data(row, year, 1, None, 'GP'),
-                                                                error_catch_input_data(row, year, 2, None, 'GP'),
-                                                                error_catch_input_data(row, year, 3, None, 'GP'),
-                                                                error_catch_input_data(row, year, 4, None, 'GP'),
-                                                                error_catch_input_data(row, year, 5, None, 'GP'),
-                                                                error_catch_input_data(row, year, 1, 'ev', 'ATOI'),
-                                                                error_catch_input_data(row, year, 2, 'ev', 'ATOI'),
-                                                                error_catch_input_data(row, year, 3, 'ev', 'ATOI'),
-                                                                error_catch_input_data(row, year, 4, 'ev', 'ATOI'),
-                                                                error_catch_input_data(row, year, 5, 'ev', 'ATOI')
-                ]
-
-    if download_file == True:
-        filename = f'instance_training_data'
-        if not os.path.exists(f'{os.path.dirname(__file__)}/CSV Data'):
-            os.makedirs(f'{os.path.dirname(__file__)}/CSV Data')
-        instance_df.to_csv(f'{os.path.dirname(__file__)}/CSV Data/{filename}.csv')
-        print(f'{filename}.csv has been downloaded to the following directory: {os.path.dirname(__file__)}/CSV Data')
-
-    return instance_df
-
-def error_catch_input_data(row, year, yX, situation, stat):
-    situation_reassignment = {'ev': 'EV', '5v5': '5v5', 'pp': 'PP', 'pk': 'PK'}
-    try:
-        if situation == None:
-            result = row[f'{year+yX-4} {stat}']
-        else:
-            result = row[f'{year+yX-4} {situation_reassignment[situation]} {stat}']
-    except KeyError:
-        result = np.nan
-    return result
-
 def scrape_player_statistics(existing_csv=False):
     if existing_csv == True:
         player_bio_df = pd.read_csv(f"{os.path.dirname(__file__)}/CSV Data/player_bios.csv")
     elif existing_csv == False:
         player_bio_df = scrape_bios(False)
-    player_bio_df = player_bio_df.drop(player_bio_df.columns[0], axis=1)
+        player_bio_df = player_bio_df.drop(player_bio_df.columns[0], axis=1)
 
     if existing_csv == True:
         stat_df = pd.read_csv(f"{os.path.dirname(__file__)}/CSV Data/historical_player_statistics.csv")
@@ -205,8 +147,98 @@ def scrape_player_statistics(existing_csv=False):
         stat_df = scrape_statistics(stat_df, 'ev', 'oi', True)
         stat_df = scrape_statistics(stat_df, 'pp', 'oi', True)
         stat_df = scrape_statistics(stat_df, 'pk', 'oi', True)
-    print(stat_df)
 
     return stat_df
 
-# print(create_instance_df(stat_df, False))
+def create_instance_df(dependent_variable, model_features, stat_df, download_file=False):
+    # str, list, df, bool
+
+    start_year = 2007
+    end_year = 2023
+
+    instance_df = pd.DataFrame(columns=['Player', 'Year', 'Position', 'Age', 'Height', 'Weight', 'Draft Position', 'Y1 GP', 'Y2 GP', 'Y3 GP', 'Y4 GP', 'Y5 GP', 'Y1 ATOI', 'Y2 ATOI', 'Y3 ATOI', 'Y4 ATOI', 'Y5 ATOI', 'Y1 G/82', 'Y2 G/82', 'Y3 G/82', 'Y4 G/82', 'Y5 G/82'])
+
+    for index, row in stat_df.iterrows():
+        for year in range(start_year+4, end_year):
+            # filter out:
+                # defence
+                # players with 0 GP in Y5
+                # players with less than 50 GP in either Y4, Y3, or Y2
+                # instances where Y5 was 2011 or earlier
+
+            if np.isnan(fetch_data(row, year, 5, None, 'GP')) or np.isnan(fetch_data(row, year, 4, None, 'GP')) or np.isnan(fetch_data(row, year, 3, None, 'GP')) or np.isnan(fetch_data(row, year, 2, None, 'GP')):
+                pass
+            elif row['Position'] == 'D':
+                pass
+            elif fetch_data(row, year, 4, None, 'GP') <= 50 or fetch_data(row, year, 3, None, 'GP') <= 50 or fetch_data(row, year, 2, None, 'GP') <= 50 or fetch_data(row, year, 1, None, 'GP') <= 50:
+                pass
+            else:
+                # Age calculation
+                # Age calculated as of the October 1st of the season
+                dob = date(int(row['Date of Birth'].split('-')[0]), int(row['Date of Birth'].split('-')[1]), int(row['Date of Birth'].split('-')[2])) #year, month, date
+                target_date = date(year, 10, 1)
+                delta_days = target_date - dob
+                age = round(delta_days.days/365.24,3)
+                instance_df.loc[f"{row['Player']} {year+1}"] = [
+                    row['Player'], 
+                    year+1, row['Position'], 
+                    age, 
+                    row['Height (in)'], 
+                    row['Weight (lbs)'], 
+                    row['Overall Draft Position'], 
+                    fetch_data(row, year, 1, None, 'GP'),
+                    fetch_data(row, year, 2, None, 'GP'),
+                    fetch_data(row, year, 3, None, 'GP'),
+                    fetch_data(row, year, 4, None, 'GP'),
+                    fetch_data(row, year, 5, None, 'GP'),
+                    fetch_data(row, year, 1, 'ev', 'ATOI') + fetch_data(row, year, 1, 'pp', 'ATOI') + fetch_data(row, year, 1, 'pk', 'ATOI'),
+                    fetch_data(row, year, 2, 'ev', 'ATOI') + fetch_data(row, year, 2, 'pp', 'ATOI') + fetch_data(row, year, 1, 'pk', 'ATOI'),
+                    fetch_data(row, year, 3, 'ev', 'ATOI') + fetch_data(row, year, 3, 'pp', 'ATOI') + fetch_data(row, year, 1, 'pk', 'ATOI'),
+                    fetch_data(row, year, 4, 'ev', 'ATOI') + fetch_data(row, year, 4, 'pp', 'ATOI') + fetch_data(row, year, 1, 'pk', 'ATOI'),
+                    fetch_data(row, year, 5, 'ev', 'ATOI') + fetch_data(row, year, 5, 'pp', 'ATOI') + fetch_data(row, year, 5, 'pk', 'ATOI'),
+                    fetch_data(row, year, 1, 'ev', 'G/60')*fetch_data(row, year, 1, 'ev', 'ATOI')/60*82 + fetch_data(row, year, 1, 'pp', 'G/60')*fetch_data(row, year, 1, 'pp', 'ATOI')/60*82 +fetch_data(row, year, 1, 'pk', 'G/60')*fetch_data(row, year, 1, 'pk', 'ATOI')/60*82,
+                    fetch_data(row, year, 1, 'ev', 'G/60')*fetch_data(row, year, 2, 'ev', 'ATOI')/60*82 + fetch_data(row, year, 2, 'pp', 'G/60')*fetch_data(row, year, 2, 'pp', 'ATOI')/60*82 +fetch_data(row, year, 2, 'pk', 'G/60')*fetch_data(row, year, 2, 'pk', 'ATOI')/60*82,
+                    fetch_data(row, year, 1, 'ev', 'G/60')*fetch_data(row, year, 3, 'ev', 'ATOI')/60*82 + fetch_data(row, year, 3, 'pp', 'G/60')*fetch_data(row, year, 3, 'pp', 'ATOI')/60*82 +fetch_data(row, year, 3, 'pk', 'G/60')*fetch_data(row, year, 3, 'pk', 'ATOI')/60*82,
+                    fetch_data(row, year, 1, 'ev', 'G/60')*fetch_data(row, year, 4, 'ev', 'ATOI')/60*82 + fetch_data(row, year, 4, 'pp', 'G/60')*fetch_data(row, year, 4, 'pp', 'ATOI')/60*82 +fetch_data(row, year, 4, 'pk', 'G/60')*fetch_data(row, year, 4, 'pk', 'ATOI')/60*82,
+                    fetch_data(row, year, 1, 'ev', 'G/60')*fetch_data(row, year, 5, 'ev', 'ATOI')/60*82 + fetch_data(row, year, 5, 'pp', 'G/60')*fetch_data(row, year, 5, 'pp', 'ATOI')/60*82 +fetch_data(row, year, 5, 'pk', 'G/60')*fetch_data(row, year, 5, 'pk', 'ATOI')/60*82
+                ]
+
+    instance_df['Y1 GP'] = instance_df['Y1 GP'].fillna(0)
+    instance_df['Y2 GP'] = instance_df['Y2 GP'].fillna(0)
+    instance_df['Y3 GP'] = instance_df['Y3 GP'].fillna(0)
+    instance_df['Y4 GP'] = instance_df['Y4 GP'].fillna(0)
+    instance_df['Y5 GP'] = instance_df['Y5 GP'].fillna(0)
+
+    if download_file == True:
+        filename = f'{dependent_variable}_instance_training_data'
+        if not os.path.exists(f'{os.path.dirname(__file__)}/CSV Data'):
+            os.makedirs(f'{os.path.dirname(__file__)}/CSV Data')
+        instance_df.to_csv(f'{os.path.dirname(__file__)}/CSV Data/{filename}.csv')
+        print(f'{filename}.csv has been downloaded to the following directory: {os.path.dirname(__file__)}/CSV Data')
+
+    return instance_df
+
+def fetch_data(row, year, yX, situation, stat):
+    situation_reassignment = {'ev': 'EV', '5v5': '5v5', 'pp': 'PP', 'pk': 'PK'}
+    try:
+        if situation == None:
+            result = row[f'{year+yX-4} {stat}']
+            if stat == 'GP':
+                if year+yX-4 == 2021:
+                    result = result/56*82
+                elif stat == 'GP' and year+yX-4 == 2020:
+                    result = result/69.5*82
+                try:
+                    result = int(result)
+                except ValueError:
+                    pass
+        else:
+            result = row[f'{year+yX-4} {situation_reassignment[situation]} {stat}']
+    except KeyError:
+        result = np.nan
+    return result
+
+stat_df = scrape_player_statistics(True)
+print(stat_df)
+forward_gp_instance_df = create_instance_df('forward_GP', [], stat_df, True)
+print(forward_gp_instance_df)
