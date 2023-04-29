57207462a393736aea4b15623db8f52175f9259a
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2023-04-29 18:14:24 -0400

    Update and rename main.py to Main/main_ADV.py

--- Main/main_ADV.py
+++ Main/main_ADV.py
@@ -10,12 +10,13 @@ import os
 
 start = time.time()
 
-input_shape = (15,)
+input_shape = (19,)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
     tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(16, activation='relu'),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(10, activation='relu'),
     tf.keras.layers.Dense(1, activation='linear')
 ])
 
@@ -32,7 +33,7 @@ X = []
 y = []
 
 for index, row in df.iterrows():
-    X.append([row['Age'], row['Height'], row['Weight'], row['Y1 GP'], row['Y2 GP'], row['Y3 GP'], row['Y4 GP'], row['Y1 ATOI'], row['Y2 ATOI'], row['Y3 ATOI'], row['Y4 ATOI'], row['Y1 G/82'], row['Y2 G/82'], row['Y3 G/82'], row['Y4 G/82']]) # features
+    X.append([row['Age'], row['Height'], row['Weight'], row['Y1 GP'], row['Y2 GP'], row['Y3 GP'], row['Y4 GP'], row['Y1 ATOI'], row['Y2 ATOI'], row['Y3 ATOI'], row['Y4 ATOI'], row['Y1 G/82'], row['Y2 G/82'], row['Y3 G/82'], row['Y4 G/82'], row['Y1 P/82'], row['Y2 P/82'], row['Y3 P/82'], row['Y4 P/82']]) # features
     y.append(row['Y5 GP']) # target
 
 X = np.array(X)
@@ -44,12 +45,12 @@ X_scaler = StandardScaler().fit(X_train)
 X_train_scaled = X_scaler.transform(X_train)
 X_test_scaled = X_scaler.transform(X_test)
 
-model.fit(X_train_scaled, y_train, epochs=100)
+model.fit(X_train_scaled, y_train, epochs=80)
 test_loss, test_acc, *rest = model.evaluate(X_test_scaled, y_test, verbose=1)
 print(f'\nMean Absolute Error of test: {test_acc:.4f}')
 
 # Make Projection
-x_new = X_scaler.transform([[26.75, 73, 193, 76, 82, 80, 82, 21.87, 22.15, 22.07, 22.39, 44, 48, 45, 64]])
+x_new = X_scaler.transform([[26.75, 73, 193, 76, 82, 80, 82, 21.87, 22.15, 22.07, 22.39, 44, 48, 45, 64, 124, 154, 126, 152]])
 y_pred = model.predict(x_new)[0][0]
 
 print(f'Projected games: {y_pred:.2f}')
