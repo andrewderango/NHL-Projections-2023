0811c6564e516260668fa55b5109f1061ebdc996
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2023-04-30 14:15:38 -0400

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -13,8 +13,10 @@ start = time.time()
 input_shape = (7,)
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
-    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(126, activation='relu', input_shape=input_shape),
+    tf.keras.layers.Dense(42, activation='relu'),
+    tf.keras.layers.Dense(14, activation='relu'),
+    tf.keras.layers.Dense(6, activation='relu'),
     tf.keras.layers.Dense(1, activation='linear')
 ])
 
@@ -32,24 +34,24 @@ y = []
 
 for index, row in df.iterrows():
     X.append([row['Age'], row['Height'], row['Weight'], row['Y1 GP'], row['Y2 GP'], row['Y3 GP'], row['Y4 GP']]) # features
-    y.append(row['Y5 GP']) # target
+    y.append(row['Y5 dGP']) # target
 
 X = np.array(X)
 y = np.array(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 1)
 
 X_scaler = StandardScaler().fit(X_train)
 X_train_scaled = X_scaler.transform(X_train)
 X_test_scaled = X_scaler.transform(X_test)
 
-model.fit(X_train_scaled, y_train, epochs=80)
-test_loss, test_acc, *rest = model.evaluate(X_test_scaled, y_test, verbose=1)
+model.fit(X_train, y_train, epochs=5) # model.fit(X_train_scaled, y_train, epochs=80)
+test_loss, test_acc, *rest = model.evaluate(X_test, y_test, verbose=1) # test_loss, test_acc, *rest = model.evaluate(X_test_scaled, y_test, verbose=1)
 print(f'\nMean Absolute Error of test: {test_acc:.4f}')
 
 # Make Projection
-x_new = X_scaler.transform([[30, 72, 188, 82, 70, 82, 82]])
-y_pred = model.predict(x_new)[0][0]
+x_new = [[26, 72, 188, 82, 82, 82, 82]] # x_new = X_scaler.transform([x, x, x, x, x])
+y_pred = model.predict(x_new)[0][0] + (82+82+82+82)/4
 
 print(f'Projected games: {y_pred:.2f}')
 
