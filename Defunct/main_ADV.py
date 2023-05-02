91c11e4bac60a77bff8c9d26be045ebb37dafdf9
Andrew De Rango <93727693+andrewderango@users.noreply.github.com>
2023-05-01 21:36:18 -0400

    Update main_ADV.py

--- Defunct/main_ADV.py
+++ Defunct/main_ADV.py
@@ -1,40 +1,39 @@
+import time
+import os
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib as plt
-import time
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import os
+import preprocessing_training_functions
+
+def make_projection(model, X_scaler):
+    return
 
 start = time.time()
 
-input_shape = (19,)
-
+input_shape = (7,)
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=input_shape),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(24, activation='relu'),
-    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(16, activation='relu', input_shape=input_shape),
+    tf.keras.layers.Dense(4, activation='relu'),
     tf.keras.layers.Dense(1, activation='linear')
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['MeanAbsoluteError', 'MeanSquaredLogarithmicError'])
+model.compile(optimizer='adam', loss='MeanAbsoluteError', metrics=['mean_squared_error', 'MeanSquaredLogarithmicError'])
 
 df = pd.read_csv(f'{os.path.dirname(__file__)}/CSV Data/forward_GP_instance_training_data.csv')
-# df = df.dropna()
-df = df.fillna(0)
-df = df.reset_index(drop=True)
+df = df.loc[(df['Y1 GP'] >= 60) & (df['Y2 GP'] >= 60) & (df['Y3 GP'] >= 60) & (df['Y4 GP'] >= 60)]
 print(df)
 
 X = []
 y = []
 
 for index, row in df.iterrows():
-    X.append([row['Age'], row['Height'], row['Weight'], row['Y1 GP'], row['Y2 GP'], row['Y3 GP'], row['Y4 GP'], row['Y1 ATOI'], row['Y2 ATOI'], row['Y3 ATOI'], row['Y4 ATOI'], row['Y1 G/82'], row['Y2 G/82'], row['Y3 G/82'], row['Y4 G/82'], row['Y1 P/82'], row['Y2 P/82'], row['Y3 P/82'], row['Y4 P/82']]) # features
-    y.append(row['Y5 GP']) # target
+    X.append([row['Age'], row['Height'], row['Weight'], row['Y1 GP'], row['Y2 GP'], row['Y3 GP'], row['Y4 GP']]) # features
+    y.append(row['Y5 dGP']) # target
 
 X = np.array(X)
 y = np.array(y)
@@ -45,13 +44,15 @@ X_scaler = StandardScaler().fit(X_train)
 X_train_scaled = X_scaler.transform(X_train)
 X_test_scaled = X_scaler.transform(X_test)
 
-model.fit(X_train_scaled, y_train, epochs=80)
+model.fit(X_train_scaled, y_train, epochs=5)
 test_loss, test_acc, *rest = model.evaluate(X_test_scaled, y_test, verbose=1)
-print(f'\nMean Absolute Error of test: {test_acc:.4f}')
+print(f'\nMean Absolute Error of test: {test_loss:.4f}')
+
+preprocessing_training_functions.permutation_feature_importance(model, X_test_scaled, y_test)
 
 # Make Projection
-x_new = X_scaler.transform([[26.75, 73, 193, 76, 82, 80, 82, 21.87, 22.15, 22.07, 22.39, 44, 48, 45, 64, 124, 154, 126, 152]])
-y_pred = model.predict(x_new)[0][0]
+x_new = X_scaler.transform([[29, 70, 188, 20, 20, 20, 20]])
+y_pred = model.predict(x_new)[0][0] + (20+20+20+20)/4
 
 print(f'Projected games: {y_pred:.2f}')
 
